7d2d02926f6722b399ca5f7b4ed09cb00f63e688
Matej <matejgeljic98@gmail.com>
2019-11-04 19:00:08 +0100

    sending products with queries

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,11 @@ const {
   productById,
   get,
   remove,
-  update
+  update,
+  list,
+  listRelated,
+  listBySearch,
+  photo
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -27,6 +31,10 @@ router.put(
   isAdmin,
   update
 );
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.param("userId", userById);
 router.param("productId", productById);
--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-const { userById } = require("../controllers/user");
+const { userById, get, update } = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile
   });
 });
+
+router.get("/user/:userId", requireSignin, isAuth, get);
+router.put("/user/:userId", requireSignin, isAuth, update);
 
 router.param("userId", userById);
 
